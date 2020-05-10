d032c223320209fb6aa67b4fe0ac94ee373ff552
Alexander Sakhaev <alsakhaev@gmail.com>
2020-05-10 19:43:33 +0300

    fix: cors error

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,19 +1,17 @@
 import express from 'express';
 import apiRouter from './routes';
 import socket from 'socket.io';
+import { Server } from 'http';
 import { socket as socketRouter } from './socket';
 import cors from 'cors';
 
 const app = express();
-const io = socket.listen(app);
+const server = new Server(app);
+const io = socket(server);
+
+server.listen(8081);
+
 io.origins('*:*');
 app.use(cors());
 app.use(apiRouter);
 socketRouter(io);
-
-const port = process.env.PORT || '8081';
-
-app.listen(port, err => {
-    if (err) return console.error(err);
-    return console.log(`Server is listening on ${port}`);
-});
